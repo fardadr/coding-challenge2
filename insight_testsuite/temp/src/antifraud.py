1f96ba075bc6bba34daf8afff59dfee75b6cef2e
Gene Der Su <e870252314@yahoo.com.tw>
2016-11-05 20:24:31 -0700

    feat: pre-compute second degree nodes
    Try to optimize the code by pre-compute second degree nodes. Remove the
    duplication of feature1 and feature2 in feature 3. Added
    check_intersection function for quick return of intersections.

--- insight_testsuite/temp/src/antifraud.py
+++ insight_testsuite/temp/src/antifraud.py
@@ -2,7 +2,7 @@
 
 import sys
 import copy
-
+import time
 
 class Graph:
     """
@@ -31,29 +31,62 @@ class Graph:
                     self.graph[id2].add(id1)
                 else:
                     self.graph[id2] = set([id1])
+        
+        self.graph2 = {}
+        for i in self.graph:
+            self.graph2[i] = self.graph[i]
+            for j in self.graph[i]:
+                self.graph2[i].union(self.graph[j])
+            self.graph2[i].add(i)
 
                     
     def feature1(self, id1, id2):
         """
             Check whether id1 and id2 are connected in the graph.
-            Return "trusted" if they are, "unverified" if not.
+            Return True if they are, False if not.
         """
         if id1 in self.graph:
             if id2 in self.graph[id1]:
-                return "trusted\n"
-        return "unverified\n"
+                return True
+        return False
 
-
+        
     def feature2(self, id1, id2):
         """
             Check whether id1 and id2 has common connection the graph.
             Return "trusted" if they are, "unverified" if not.
         """
-        # id1 and id2 might not be in the graph
-        if id1 in self.graph and id2 in self.graph:
-            if len(self.graph[id1].intersection(self.graph[id2])) > 0:
-                return "trusted\n"
-        return "unverified\n"
+##        # check for the first degree connection
+##        if self.feature1(id1, id2) == "trusted\n":
+##            return "trusted\n"
+##    
+##        # id1 and id2 might not be in the graph
+##        if id1 in self.graph and id2 in self.graph:
+##            if self.check_intersection(self.graph[id1],self.graph[id2]):
+##                return "trusted\n"
+##        return "unverified\n"
+    
+        """
+            Check whether id2 is within 2 degree of separations
+            of id1 using per-computed second degree graph. Return
+            True if they are, False if not.
+        """
+        # id1 might not be in the graph
+        if id1 in self.graph2:
+            if id2 in self.graph2[id1]:
+                return True
+        return False
+        
+
+    def check_intersection(self, set1, set2):
+        """
+            Given 2 sets, return True if there is at least one
+            interection, else return False.
+        """
+        for i in set1:
+            if i in set2:
+                return True
+        return False
 
 
     def feature3(self, id1, id2):
@@ -63,28 +96,44 @@ class Graph:
             finding the common second degree vertices from both ends. 
             Return "trusted" if they are, "unverified" if not.
         """
-        # check id1 might not be in the graph
-        if id1 in self.graph:
-            # find all vertices within 2 degree of separation from id1
-            set1 = self.graph[id1]
-            for i in copy.copy(set1):
-                set1 = set1.union(self.graph[i])
-        else:
-            return "unverified\n"
+        
+##        # check for the first degree of separation
+##        if self.feature1(id1, id2) == "trusted\n":
+##            return "trusted\n"
+##        
+##        # check id1 might not be in the graph
+##        if id1 in self.graph:
+##            # find all vertices within 2 degree of separation from id1
+##            set1 = self.graph[id1]
+##            for i in copy.copy(set1):
+##                set1 = set1.union(self.graph[i])
+##            set1.add(id1)
+##        else:
+##            return "unverified\n"
+##
+##        # check if id2 in second degree of separations
+##        if id2 in set1:
+##            return "trusted\n"
+##
+##        # check id2 might not be in the graph
+##        if id2 in self.graph:
+##            # find all vertices within 2 degree of separation from id2
+##            set2 = self.graph[id2]
+##            for i in copy.copy(set2):
+##                set2 = set2.union(self.graph[i])
+##            set2.add(id2)
+##        else:
+##            return "unverified\n"
+##        # check if there are common 2 second degree of separations
+##        if self.check_intersection(set1, set2):
+##            return "trusted\n"
+##        return "unverified\n"
+        
+        if self.check_intersection(self.graph2[id1], self.graph2[id2]):
+            return True
+        return False
 
-        # check id2 might not be in the graph
-        if id1 in self.graph:
-            # find all vertices within 2 degree of separation from id2
-            set2 = self.graph[id2]
-            for i in copy.copy(set2):
-                set2 = set2.union(self.graph[i])
-        else:
-            return "unverified\n"
-
-        # check if there are common 2 second degree of separations
-        if len(set1.intersection(set2)):
-            return "trusted\n"
-        return "unverified\n"
+        
 
 
 def main(batch_data, stream_data, output1, output2, output3):
@@ -94,20 +143,45 @@ def main(batch_data, stream_data, output1, output2, output3):
         and output3 line by line. All the arguments are assumed to be
         vaild file path. 
     """
+    t0 = time.time()
     batch_graph = Graph(batch_data)
-    
+    print "\nTime used to create graph: %ss\n"%(time.time() - t0)
+
+    t0 = time.time()
+    counter = 0
     with open(stream_data,'r') as stream, \
          open(output1,'w') as f1,\
          open(output2,'w') as f2,\
          open(output3,'w') as f3:
         next(stream)
+        
+        t0 = time.time()
+        
         for row in stream:
             row_split = row.strip().split(',')
             id1 = row_split[1]
             id2 = row_split[2]
-            f1.write(batch_graph.feature1(id1,id2))
-            f2.write(batch_graph.feature2(id1,id2))
-            f3.write(batch_graph.feature3(id1,id2))
+            current_feature1 = batch_graph.feature1(id1,id2)
+            if batch_graph.feature1(id1,id2):
+                f1.write("trusted\n")
+                f2.write("trusted\n")
+                f3.write("trusted\n")
+            else:
+                f1.write("unverified\n")
+                if batch_graph.feature2(id1,id2):
+                    f2.write("trusted\n")
+                    f3.write("trusted\n")
+                else:
+                    f2.write("unverified\n")
+                    if batch_graph.feature3(id1,id2):
+                        f3.write("trusted\n")
+                    else:
+                        f3.write("unverified\n")
+            counter += 1
+            if counter % 100000 == 0:
+                print "Row count:", counter
+
+    print "\nAll feature completed, total %s rows, average time %s s/row"%(counter, (time.time() - t0)/counter)
 
 
 if __name__ == "__main__":
